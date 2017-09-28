efb639d1f7efbb49e31cff72f7fa9912601dccb3
Martyniuk <oleksandr.martyniuk@ria.com>
2017-09-28 22:52:50 +0300

    add second screen

--- app/src/main/java/com/example/steptothefuture/MainActivity.java
+++ app/src/main/java/com/example/steptothefuture/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.steptothefuture;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button search;
+
+    public void go_to_second () {
+
+        search = (Button)findViewById(R.id.button_search);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent abc = new Intent(MainActivity.this, second_screen.class);
+                startActivity(abc);
+            }
+        });
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        go_to_second ();
     }
 }
